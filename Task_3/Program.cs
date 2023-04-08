0d226028e84280895c351b85807c9b8aca3c2f39
Maksim <komarovmu@gmail.com>
2023-04-09 00:17:37 +0100

    Task 3

---
+++ Task_3/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+
+namespace CubeTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число N: ");
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= N; i++)
+            {
+                int cube = i * i * i;
+                Console.Write(cube + ", ");
+            }
+        }
+    }
+}
